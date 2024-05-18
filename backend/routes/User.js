bb5a68eb95fc5412ac7329f8f581ebdb1ce1fb6e
Dishant <dishantd442442@gmail.com>
2024-05-18 19:02:37 +0530

    update route completed

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -4,6 +4,7 @@ const zod = require("zod");
 const { User } = require("../database/db");
 const { JWT_SECRET } = require("../config");
 const jwt = require("jsonwebtoken");
+const { authMiddleware } = require("../middleware");
 
 //signup route
 const signupBody = zod.object({
@@ -15,15 +16,20 @@ const signupBody = zod.object({
 
 router.post("/signup", async function (req, res) {
   const { success } = signupBody.safeParse(req.body);
+
   if (!success) {
-    return res
-      .status(411)
-      .json({ error: "email already exist / Incorrect Inputs" });
+    return res.status(411).json({ error: "Incorrect Inputs" });
   }
+
+  //check if user already exist
+
   const existingUser = await User.findOne(req.body.username);
   if (existingUser) {
     return res.json({ message: "user already exist" });
   }
+
+  //create a new user
+
   const newUser = await User.create({
     username: req.body.username,
     password: req.body.password,
@@ -31,7 +37,6 @@ router.post("/signup", async function (req, res) {
     lastName: req.body.lastName,
   });
   const userId = newUser._id;
-
   const token = jwt.sign(
     {
       userId,
@@ -47,7 +52,7 @@ router.post("/signup", async function (req, res) {
 // route for signin
 
 const signinBody = zod.object({
-  username: zod.string().email,
+  username: zod.string().email(),
   password: zod.string(),
 });
 
@@ -76,4 +81,24 @@ router.post("/signin", async function (req, res) {
   });
 });
 
+//route for updating the user
+
+const updateBody = zod.object({
+  password: zod.string().optional(),
+  firstName: zod.string().optional(),
+  lastName: zod.string().optional(),
+});
+
+router.put('/',authMiddleware,async (req, res) => {
+  const {success} = updateBody.safeParse(req.body);
+  if (!success) {
+      return res.status(403).json({message:"invalid input"});
+  }
+  const update = await User.updateOne(req.body,{
+    id:req.userId
+  }) 
+  res.json({
+    message: "Updated successfully",
+  });
+});
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const userRouter = require('./User');
 
+
 Router.use('/user',userRouter)
 
 module.exports = router;
