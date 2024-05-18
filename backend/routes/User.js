0928a22ef55a1067be3c43176ab4924f4fa558aa
Dishant <dishantd442442@gmail.com>
2024-05-18 17:32:42 +0530

    signin route completed

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const zod = require("zod");
 const { User } = require("../database/db");
-const {JWT_SECRET} = require("../config")
-const jwt = require("jsonwebtoken")
-
+const { JWT_SECRET } = require("../config");
+const jwt = require("jsonwebtoken");
 
 //signup route
 const signupBody = zod.object({
@@ -33,13 +32,48 @@ router.post("/signup", async function (req, res) {
   });
   const userid = newUser._id;
 
-  const token = jwt.sign({
-    userid
-  },JWT_SECRET)
+  const token = jwt.sign(
+    {
+      userid,
+    },
+    JWT_SECRET
+  );
   res.json({
-    message:"User Created Successfully",
-    token:token
-  })
+    message: "User Created Successfully",
+    token: token,
+  });
+});
+
+// route for signin
+
+const signinBody = zod.object({
+  username: zod.string().email,
+  password: zod.string(),
+});
+
+router.post("/signin", async function (req, res) {
+  const { success } = signinBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "wrong inputs",
+    });
+  }
+  const user = await User.findOne(req.body.username);
+  if (user) {
+    const token = jwt.sign(
+      {
+        userid: user._id,
+      },
+      JWT_SECRET
+    );
+    res.json({
+      token: token,
+    });
+    return;
+  }
+  res.status(411).json({
+    message: "Error while logging in",
+  });
 });
 
 module.exports = router;
