80ffeb2397f85f4b88054c669aa805d3ee24e162
Dishant <dishantd442442@gmail.com>
2024-05-19 11:07:42 +0530

    created route to fetch all the user from the db

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -101,4 +101,32 @@ router.put('/',authMiddleware,async (req, res) => {
     message: "Updated successfully",
   });
 });
+
+// route to get user 
+
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+
+  const users = await User.find({
+      $or: [{
+          firstName: {
+              "$regex": filter
+          }
+      }, {
+          lastName: {
+              "$regex": filter
+          }
+      }]
+  })
+
+  res.json({
+      user: users.map(user => ({
+          username: user.username,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          _id: user._id
+      }))
+  })
+})
+
 module.exports = router;
