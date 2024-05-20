8c29bd1b16dc5ca9b5531ed1a4a3f854b2287bbd
Dishant <dishantd442442@gmail.com>
2024-05-21 00:40:08 +0530

    installed tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Signup, Signin, Dashboard, SendMoney } from './components/index';
+import React from 'react'
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />    
-        <Route path="/signin" element={<Signin />} />    
-        <Route path="/dashboard" element={<Dashboard />} />    
-        <Route path="/sendmoney" element={<SendMoney />} />    
-      </Routes>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />    
+          <Route path="/signin" element={<Signin />} />    
+          <Route path="/dashboard" element={<Dashboard />} />    
+          <Route path="/sendmoney" element={<SendMoney />} />    
+        </Routes>
     </BrowserRouter>
+      
+    </>
   );
 }
 
