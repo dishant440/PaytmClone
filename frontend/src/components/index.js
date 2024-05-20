25e94f56667003e28e541f702ce1e94b7d82fd74
Dishant <dishantd442442@gmail.com>
2024-05-21 00:23:39 +0530

    created route in frontend

---
+++ frontend/src/components/index.js
@@ -0,0 +1,6 @@
+export {default as SendMoney} from './SendMoney';
+export { default as Signup } from './Signup';
+export { default as Signin } from './Signin';
+export { default as Dashboard } from './Dashboard';
+
+
